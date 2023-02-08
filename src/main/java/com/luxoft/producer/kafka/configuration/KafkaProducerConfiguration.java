2a1b771febc0bbbac4ab89b52ddd581c4e79b576
DGodoyChiclana <david.godoychiclana@dxc.com>
2023-02-08 13:49:57 +0100

    Now the application can be converted into an image compatible with docker-compose configuration.

--- src/main/java/com/luxoft/producer/kafka/configuration/KafkaProducerConfiguration.java
+++ src/main/java/com/luxoft/producer/kafka/configuration/KafkaProducerConfiguration.java
@@ -3,6 +3,7 @@ package com.luxoft.producer.kafka.configuration;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.VoidSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,18 +16,26 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfiguration {
 
-    private static final String BOOTSTRAP_SERVERS = ":9092";
     private static final String CLIENT_ID = "ex";
+
+    @Value("${localhost.ip}")
+    private String LOCALHOST_IP;
+
+    @Value("${kafka.bootstrap.servers.port}")
+    private String BOOTSTRAP_SERVERS_PORT;
+
+    @Value("${kafka.schema.registry.port}")
+    private String SCHEMA_REGISTRY_PORT;
 
     @Bean
     public ProducerFactory<Void, Object> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
 
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, LOCALHOST_IP + ":" + BOOTSTRAP_SERVERS_PORT);
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        properties.put("schema.registry.url", "http://127.0.0.1:8081");
+        properties.put("schema.registry.url", "http://" + LOCALHOST_IP + ":" + SCHEMA_REGISTRY_PORT);
 
         return new DefaultKafkaProducerFactory<>(properties);
     }
